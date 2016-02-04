83e693e6a609fc4fdbb056bf07c0af7b0940c707
KSrikanth <KSrikanth@CEI3037.CEIIND.COM>
2016-02-04 16:34:25 +0530

    Sleep code Removed

--- src/DemoTourTestCases/SigninPageLink.java
+++ src/DemoTourTestCases/SigninPageLink.java
@@ -15,11 +15,7 @@ public class SigninPageLink {
 	public void TestSignin() throws InterruptedException{
 		SigninPage signin = new SigninPage(BrowserUtil.getDriver());
 		signin.navigateToSigninPage();
-		/*signin.setUserName("mercury");
-                signin.setPassword("mercury");
-                signin.setLogin();*/
 		signin.loginIn("mercury", "mercury");
-		Thread.sleep(1000);
 		Assert.assertEquals(BrowserUtil.getDriver().findElement(By.xpath("//tr[3]/td/font")).getText(), "Use our Flight Finder to search for the lowest fare on participating airlines. Once you've booked your flight, don't forget to visit the Mercury Tours Hotel Finder to reserve lodging in your destination city.");
 		Assert.assertTrue(signin.isSignOffDisplayed());
 		Thread.sleep(1000);
